4886c7b4409e26ab895aa9af03b3fca25aed02e9
khasbmsharyy <khasbmsharyy@gmail.com>
2026-06-26 17:58:57 +0300

    Update sseerr.js

--- sseerr.js
+++ sseerr.js
@@ -422,7 +422,7 @@ $("<style>#audio-container { position: absolute;bottom: 0;width: 100%;background
     $("div#tlogins .btn.btn-primary").click(function () { $(".a3").hide(); });
 
     $(
-        '<div id="color-picker-icon"><img src="https://up6.cc/2026/06/178246155941331.png" alt="Color Picker Icon"></div>' +
+        '<div id="color-picker-icon"><img src="1234.png" alt="Color Picker Icon"></div>' +
         '<div id="color-picker-panel">' +
         '<h4>اختر لون الخلفية</h4><div class="custom-color">' +
         '<div style="background-color: #a41e54;" data-color="#a41e54"></div><div style="background-color: #982072;" data-color="#982072"></div><div style="background-color: #98278f;" data-color="#98278f"></div><div style="background-color: #62007a;" data-color="#62007a"></div><div style="background-color: #3c007a;" data-color="#3c007a"></div><div style="background-color: #0d1c4d;" data-color="#0d1c4d"></div><div style="background-color: #6f6f6f;" data-color="#6f6f6f"></div><div style="background-color: #212121;" data-color="#212121"></div></div>' +
@@ -509,7 +509,7 @@ $("[data-target=#rooms]").attr('title','   غــــــــــرف  ســر 
 $("[data-target=#chats]").attr('title','      خــــــــــــــاص  ســر قــلــبــي ')
 $("[data-target=#users]").attr('title',' اعـــــضـــــاء ســر قــلــبــي    ')
 
-$('<img style="height:60px;width: 100%;margin-right: 50px;background-color:#595A5C;"src="https://up6.cc/2026/06/178246103354271.jpg"border="0">').insertBefore('div#d2bc');    
+$('<img style="height:60px;width: 100%;margin-right: 50px;background-color:#595A5C;"src="123123.jpg"border="0">').insertBefore('div#d2bc');    
     // 4. تعديل النصوص الترحيبية
     $('.fl.ustat').css('width', '3px');
     $('.fa.fa-user-plus').html(`❥تسجيل `);
